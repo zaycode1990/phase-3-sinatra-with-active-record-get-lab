a747f1a3922c9fbe3197460569432c5863d3b30f
Isaiah <ijandicos90@gmail.com>
2022-08-07 03:34:53 -0400

    commit tree ce3daccfc7b3ffee87c8a01135db3bc78294e82d

--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,16 @@
 class BakedGood < ActiveRecord::Base
   # add association macro here
+  belongs_to :bakery
+
+  # This method could also be written using a scope:
+  # https://guides.rubyonrails.org/active_record_querying.html#scopes
+  # scope :by_price, -> { order(price: :desc) }
+  def self.by_price
+    self.all.order(price: :desc)
+  end
+
+  def self.most_expensive
+    self.all.order(price: :desc).first
+  end
+
 end
